5d9a0ccd776b6ddd8c732754e8132f75b6e0aef2
Chris Loftus <cwl@aber.ac.uk>
2020-08-17 08:31:54 +0100

    Added the Navigation graph, two blank fragments and linked the graph with the BottomNavigationView menu

--- app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/MainActivity.kt
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/MainActivity.kt
@@ -1,10 +1,19 @@
 package uk.ac.aber.dcs.cs31620.faa.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import uk.ac.aber.dcs.cs31620.faa.R
 import uk.ac.aber.dcs.cs31620.faa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +23,17 @@ class MainActivity : AppCompatActivity() {
 
         val toolbar = binding.toolbar
         setSupportActionBar(toolbar)
+
+        val navView = binding.bottomNavView
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home, R.id.navigation_cats))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
+
+
 }
---
+++ app/src/main/java/uk/ac/aber/dcs/cs31620/faa/ui/cats/CatsFragment.kt
@@ -0,0 +1,18 @@
+package uk.ac.aber.dcs.cs31620.faa.ui.cats
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import uk.ac.aber.dcs.cs31620.faa.R
+
+class CatsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
